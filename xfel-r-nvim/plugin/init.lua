5c12110a4720567ecae60c6547d594b7eca91487
Fernando Jascovich <fernando.ej@gmail.com>
2024-07-07 13:31:56 -0300

    Rmarkdown

--- xfel-r-nvim/plugin/init.lua
+++ xfel-r-nvim/plugin/init.lua
@@ -1,27 +1,59 @@
 local env_fname = '/tmp/env_file.Rdata'
-local post = 'save.image("' .. env_fname .. '")'
+local load_env = 'load("' .. env_fname .. '")'
+local save_env = 'save.image("' .. env_fname .. '")'
+local script_before = {}
 
-local Rexecute = function(opts)
+local execute_contents = function(contents) 
   local script = vim.fn.tempname()
-  local pre = 'load("' .. env_fname .. '")'
-  local contents = { pre, opts.fargs[1], post }
   vim.fn.writefile(contents, script)
   local result = vim.fn.system({ 'Rscript', script })
   print(result)
 end
 
+local Rexecute = function(opts)
+  local contents = { load_env }
+  for _, added in ipairs(script_before) do
+    table.insert(contents, added)
+  end
+  table.insert(contents, opts.fargs[1])
+  table.insert(contents, save_env)
+  execute_contents(contents)
+end
+
 local Rclear = function() 
-  vim.fn.system({ 'Rscript', '-e', post })
+  vim.fn.system({ 'Rscript', '-e', save_env })
+  script_before = {} 
+end
+
+local Rmarkdown = function() 
+  local fname = vim.fn.expand("%")
+  local contents = {
+    'library(rmarkdown)',
+    'rmarkdown::render("' .. fname .. '")'
+  }
+  execute_contents(contents)
+end
+
+local Radd = function(opts)
+  table.insert(script_before, opts.fargs[1])
+  print(table.concat(script_before, "\n"))
 end
 
 vim.api.nvim_create_autocmd({"BufEnter", "BufWinEnter"}, {
-  pattern = "*.Rmd",
+  pattern = { "*.R", "*.Rmd" },
   callback = function()
     vim.api.nvim_create_user_command(
       'Rexecute', 
-      Rexecute, 
-      { nargs = 1 }
+      Rexecute, { nargs = 1 }
     )
     vim.api.nvim_create_user_command('Rclear', Rclear, {})
+    vim.api.nvim_create_user_command('Radd', Radd, { nargs = 1 })
+  end
+})
+
+vim.api.nvim_create_autocmd({"BufEnter", "BufWinEnter"}, {
+  pattern = { "*.Rmd" },
+  callback = function()
+    vim.api.nvim_create_user_command('Rmarkdown', Rmarkdown, {})
   end
 })
